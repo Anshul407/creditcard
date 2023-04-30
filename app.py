9dfe25584fdc516890fb60bdcb1a5989f003f70d
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2023-04-30 21:57:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -117,13 +117,12 @@ with col3:
     st.header('Amount', style=color)
     amount = st.number_input('', step=0.001)
 
-# creating a button to predict the transaction
-submit_button = st.form_submit_button(label='Predict')
-if submit_button:
-    input_data = [time, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19,v20, v21, v22, v23, v24, v25, v26, v27, v28, amount]
-    prediction = finalmodel.predict([input_data])
-
-# displaying the prediction result
-    st.write(f"The predicted transaction result is {prediction[0]}")
+# creating a button for prediction
+if st.button('Check for Fraudulent Transaction'):
+    prediction = finalmodel.predict([[time, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27, v28, amount]])
+    if prediction[0] == 1:
+        st.error('The details you entered have a fraudulent transaction.')
+    else:
+        st.success('The transaction is valid.')
     
 
