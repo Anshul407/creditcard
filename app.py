1d12a1a89cdfa70beb29e463dd6e2cf0ef5239d4
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2023-04-30 22:05:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,123 +2,136 @@
 
 import pickle
 import streamlit as st
+from streamlit_option_menu import option_menu
 
-# loading the saved model
+
+# loading the saved models
+
 finalmodel = pickle.load(open('model_file.sav', 'rb'))
 
-# page title
-st.set_page_config(page_title='Fake Credit Card Transaction Prediction', page_icon=':credit_card:')
 
 # sidebar for navigation
-st.sidebar.title('Navigation')
-options = st.sidebar.selectbox('', ('Credit Card Fraudlent Transaction Prediction', ))
-
-# getting the input data from the user
-st.title('Transaction Prediction')
-st.write('Please enter the following details to check for fraudulent transactions:')
-
-color = 'gray'
-
-# creating input boxes for transaction details
-with st.form(key='credit_card_details'):
+with st.sidebar:
+    
+    selected = option_menu('Credit Card ',
+                          
+                          ['Credit Card'],
+                          icons=['activity'],
+                          default_index=0)
+    
+    
+if (selected == 'Transaction Prediction'):
+    
+    # page title
+    st.title('Fake Credit Card Transaction Prediction')
+    
+    
+    # getting the input data from the user
     col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        Pregnancies = st.text_input('Time')
+        
+    with col2:
+        Glucose = st.text_input('V1')
+    
+    with col3:
+        BloodPressure = st.text_input('V2')
+    
+    with col1:
+        SkinThickness = st.text_input('V3')
+    
+    with col2:
+        Insulin = st.text_input('V4')
+    
+    with col3:
+        BMI = st.text_input('V5')
+    
+    with col1:
+        DiabetesPedigreeFunction = st.text_input('V6')
+    
+    with col2:
+        Age = st.text_input('V7')
+    with col3:
+        a = st.text_input('V8')
+    
+    with col1:
+        b = st.text_input('V9')
+    
+    with col2:
+        c = st.text_input('V10')
+    
+    with col3:
+        d = st.text_input('V11')
+    
+    with col1:
+        e = st.text_input('V12')
+    
+    with col2:
+        f= st.text_input('V13')
+    with col3:
+        g= st.text_input('V14')
+    
+    with col1:
+        h = st.text_input('V15')
+    
+    with col2:
+        i= st.text_input('V16')
+        
+    with col3:
+        j= st.text_input('V17')
 
     with col1:
-        st.header('Time', style=color)
-        time = st.number_input('', min_value=0, step=1)
-
-        st.header('V1', style=color)
-        v1 = st.number_input('', step=0.001)
-
-        st.header('V4', style=color)
-        v4 = st.number_input('', step=0.001)
-
-        st.header('V5', style=color)
-        v5 = st.number_input('', step=0.001)
-
-        st.header('V6', style=color)
-        v6 = st.number_input('', step=0.001)
-
-        st.header('V8', style=color)
-        v8 = st.number_input('', step=0.001)
-
-        st.header('V9', style=color)
-        v9 = st.number_input('', step=0.001)
-
-        st.header('V11', style=color)
-        v11 = st.number_input('', step=0.001)
-
-        st.header('V12', style=color)
-        v12 = st.number_input('', step=0.001)
-
-        st.header('V13', style=color)
-        v13 = st.number_input('', step=0.001)
-
-        st.header('V14', style=color)
-        v14 = st.number_input('', step=0.001)
-
-        st.header('V16', style=color)
-        v16 = st.number_input('', step=0.001)
-
-        st.header('V17', style=color)
-        v17 = st.number_input('', step=0.001)
-
-        st.header('V18', style=color)
-        v18 = st.number_input('', step=0.001)
-
-        st.header('V19', style=color)
-        v19 = st.number_input('', step=0.001)
-
-        st.header('V21', style=color)
-        v21 = st.number_input('', step=0.001)
-
-        st.header('V23', style=color)
-        v23 = st.number_input('', step=0.001)
-
-        st.header('V25', style=color)
-        v25 = st.number_input('', step=0.001)
-
-        st.header('V26', style=color)
-        v26 = st.number_input('', step=0.001)
-
-        st.header('V27', style=color)
-        v27 = st.number_input('', step=0.001)
-
+        k = st.text_input('V18')
+    
     with col2:
-        st.header('V2', style=color)
-        v2 = st.number_input('', step=0.001)
+        l= st.text_input('V19')
+        
+    with col3:
+        m= st.text_input('V20')
+     
+    with col1:
+        n = st.text_input('V21')
+    
+    with col2:
+        o= st.text_input('V22')
+        
+    with col3:
+        p= st.text_input('V23')
 
-        st.header('V3', style=color)
-        v3 = st.number_input('', step=0.001)
+    with col1:
+        q = st.text_input('V24')
+    
+    with col2:
+        r= st.text_input('V25')
+        
+    with col3:
+        s= st.text_input('V26')
 
-        st.header('V7', style=color)
-        v7 = st.number_input('', step=0.001)
+    with col1:
+        t = st.text_input('V27')
+    
+    with col2:
+        u= st.text_input('V28')
+        
+    with col3:
+        v= st.text_input('Amount')
+    
+    
+    
+    
+    
+    # code for Prediction
+    trans_result = ''
+    
+    # creating a button for Prediction
+    
+    if st.button('Transaction Prediction Result'):
+        trans_prediction = finalmodel.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v]])
+        if (trans_prediction[0] == 1):
+          trans_result = 'The Details you entered have fraudlent transaction'
+        else:
+          trans_result = 'Valid Transaction'
 
-        st.header('V10', style=color)
-        v10 = st.number_input('', step=0.001)
-with col3:
-    st.header('V15', style=color)
-    v15 = st.number_input('', step=0.001)
-
-    st.header('V20', style=color)
-    v20 = st.number_input('', step=0.001)
-
-    st.header('V22', style=color)
-    v22 = st.number_input('', step=0.001)
-
-    st.header('V24', style=color)
-    v24 = st.number_input('', step=0.001)
-
-    st.header('V28', style=color)
-    v28 = st.number_input('', step=0.001)
-
-    st.header('Amount', style=color)
-    amount = st.number_input('', step=0.001)
-# creating a button for prediction
-submit_button = st.form_submit_button(label='Predict')
-if submit_button:
-    prediction = finalmodel.predict([[time, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27, v28, amount]])
-    if prediction[0] == 1:
-        st.error('The details you entered have a fraudulent transaction.')
-    else:
-        st.success('The transaction is valid.') 
+        
+    st.success(trans_result)
